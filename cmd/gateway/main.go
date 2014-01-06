978fda340e04e4506e280aff5e182c81a8a74a32
Xing Xing <mikespook@gmail.com>
2014-01-06 12:09:45 +0800

    removed tls

--- cmd/gateway/main.go
+++ cmd/gateway/main.go
@@ -8,16 +8,11 @@ import (
 	"github.com/mikespook/x/gateway"
 	"os"
 	"syscall"
-	"net"
-)
-
-const (
-	GITLAB = "gitlab"
-	GITHUB = "github"
 )
 
 var (
-	addr       = flag.String("addr", "127.0.0.1:8081", "Address of socket")
+	netname	= flag.String("net", "tcp", "Network interface")
+	addr       = flag.String("addr", "127.0.0.1:8081", "Address of gateway")
 	tlsCert       = flag.String("tls-cert", "", "TLS cert file")
 	tlsKey        = flag.String("tls-key", "", "TLS key file")
 	pf         = flag.String("pid", "", "PID file")
@@ -44,32 +39,24 @@ func main() {
 			log.Messagef("PID: %d file=%q", p.Pid, *pf)
 		}
 	}
+	// Init Gateway
+	gw := gateway.New(*netname, *addr)
+	gw.SetLogger(log.DefaultLogger)
+	// Goroutine a Gateway logical
+	go func() {
+		if err := gw.Serve(); err != nil {
+			log.Error(err)
+			signal.Send(os.Getpid(), os.Interrupt)
+		}
+	}()
 	defer func() {
 		log.Message("Exited!")
 		log.WaitClosing()
 	}()
-	// Init Gateway
-	gw := gateway.New(*addr)
-	if *tlsCert != "" && *tlsKey != "" {
-		// Use TLS connection
-		log.Messagef("Using TLS: cert=%q; key=%q", *cert, *key)
-		if err := gw.SetTLS(*tlsCert, *tlsKey); err != nil {
-			log.Error(err)
-			return
-		}
-	}
-	defer gw.Close()
-	// Goroutine a Gateway logical
-	go func() {
-		if err := gw.Serve(); err != nil {
-			if _, ok := err.(*net.OpError); !ok {
-				log.Error(err)
-				signal.Send(os.Getpid(), os.Interrupt)
-			}
-		}
-	}()
 	sh := signal.NewHandler()
-	sh.Bind(os.Interrupt, func() bool { return true })
+	sh.Bind(os.Interrupt, func() bool {
+		return true
+	})
 	sh.Bind(syscall.SIGUSR1, func() bool {
 		// Shutdown Gateway
 		gw.Close()
--- gateway/gateway.go
+++ gateway/gateway.go
@@ -1,19 +1,79 @@
 package gateway
 
-type Gateway struct {}
+import (
+	"net"
+	"bufio"
+	"github.com/mikespook/golib/log"
+)
 
-func New(addr string) (gw *Gateway) {
-	return &Gateway{}
+type Gateway struct {
+	netname, addr string
+	listener net.Listener
+	logger *log.Logger
 }
 
-func(gw *Gateway) SetTLS(cert, key string) (err error) {
-	return nil
+func New(netname, addr string) (gw *Gateway) {
+	return &Gateway{
+		netname: netname,
+		addr: addr,
+	}
+}
+
+func(gw *Gateway) msgf(format string, msg ... interface{}) {
+	if gw.logger != nil {
+		gw.logger.Messagef(format, msg ...)
+	}
+}
+
+func(gw *Gateway) err(err error) {
+	if gw.logger != nil {
+		gw.logger.Error(err)
+	}
+}
+
+func(gw *Gateway) SetLogger(logger *log.Logger) {
+	gw.logger = logger
 }
 
 func(gw *Gateway) Close() {
-	
+	if err := gw.listener.Close(); err != nil {
+		gw.err(err)
+	}
 }
 
 func(gw *Gateway) Serve() (err error) {
+	gw.listener, err = net.Listen(gw.netname, gw.addr)
+	if err != nil {
+		return err
+	}
+	for {
+		conn, err := gw.listener.Accept()
+		if err != nil {
+			gw.err(err)
+			if err.Error() != "use of closed network connection" {
+				continue
+			}
+			break
+		}
+		if conn == nil {
+			break
+		}
+		go gw.newClient(conn)
+	}
 	return nil
+}
+
+func (gw *Gateway) newClient(conn net.Conn) {
+	defer func() {
+		gw.msgf("The connection terminated: %s => %s", conn.RemoteAddr(), conn.LocalAddr())
+		if err := conn.Close(); err != nil {
+			gw.err(err)
+		}
+	}()
+	gw.msgf("New connection established: %s => %s", conn.RemoteAddr(), conn.LocalAddr())
+	r := bufio.NewReader(conn)
+	w := bufio.NewWriter(conn)
+	rw := bufio.NewReadWriter(r, w)
+	rw.WriteString("Hello")
+	rw.Flush()
 }
