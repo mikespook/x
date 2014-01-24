ed555f5c75fa7a8f70587ea44bdfa3aad71d9429
Xing Xing <mikespook@gmail.com>
2014-01-24 15:31:42 +0800

    refactoring, added the script interpreter

---
+++ lua.go
@@ -0,0 +1,55 @@
+package x
+
+import (
+	"github.com/aarzilli/golua/lua"
+	"github.com/mikespook/golib/iptpool"
+	"github.com/mikespook/golib/log"
+	"github.com/stevedonovan/luar"
+	"path"
+)
+
+const (
+	luaModule = "x"
+)
+
+type LuaIpt struct {
+	module string
+	state  *lua.State
+	path   string
+}
+
+func newLuaIpt() iptpool.ScriptIpt {
+	return &LuaIpt{}
+}
+
+func (luaipt *LuaIpt) Exec(name string, params interface{}) error {
+	f := path.Join(luaipt.path, name+".lua")
+	luaipt.Bind("Params", params)
+	return luaipt.state.DoFile(f)
+}
+
+func (luaipt *LuaIpt) Init(path string) error {
+	luaipt.state = luar.Init()
+	luaipt.Bind("Debugf", log.Debugf)
+	luaipt.Bind("Debug", log.Debug)
+	luaipt.Bind("Messagef", log.Messagef)
+	luaipt.Bind("Message", log.Message)
+	luaipt.Bind("Warningf", log.Warningf)
+	luaipt.Bind("Warning", log.Warning)
+	luaipt.Bind("Errorf", log.Errorf)
+	luaipt.Bind("Error", log.Error)
+	luaipt.path = path
+	return nil
+}
+
+func (luaipt *LuaIpt) Final() error {
+	luaipt.state.Close()
+	return nil
+}
+
+func (luaipt *LuaIpt) Bind(name string, item interface{}) error {
+	luar.Register(luaipt.state, luaModule, luar.Map{
+		name: item,
+	})
+	return nil
+}
